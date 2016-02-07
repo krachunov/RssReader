0fa0808635398041c70307765a9b2bd37dce1109
ico <ico@ico-PC>
2016-02-07 12:14:47 +0200

    make some test in demo class

--- RssReader/src/edu/pragmatic/java/advanced/rss/project/model/DemoRss.java
+++ RssReader/src/edu/pragmatic/java/advanced/rss/project/model/DemoRss.java
@@ -5,6 +5,8 @@ package edu.pragmatic.java.advanced.rss.project.model;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 import edu.pragmatic.java.advanced.rss.project.UI.StartDisplay;
@@ -13,7 +15,7 @@ import javafx.application.Application;
 public class DemoRss {
 
 	public static void main(String[] args) throws IOException {
-		// RssOption reader = new MyRssReader();
+		RssOption reader = new MyRssReader();
 		// Scanner sc = new Scanner(System.in);
 		// String inputUri = sc.nextLine();
 		// if (!inputUri.equals("")) {
@@ -21,16 +23,23 @@ public class DemoRss {
 		//
 		// }
 
-		// reader.addFeedSorce("https://sports.yahoo.com/top/rss.xml");
+		reader.addFeedSorce("https://sports.yahoo.com/top/rss.xml");
 		// reader.addFeedSorce("https://sports.yahoo.com/golf/blog/devil_ball_golf/rss.xml");
 		// reader.removeFeedSorce("https://sports.yahoo.com/golf/blog/devil_ball_golf/rss.xml");
 		// reader.addFeedSorce("http://rss.cnn.com/rss/edition.rss");
 
 		// Test displayAllSources
-		// final List<String> displayAllSources = reader.displayAllSources();
-		// for (String string : displayAllSources) {
-		// System.out.println(string);
-		// }
+		final Map<String, List<RssInfo>> desc = reader.getAllSources();
+
+		for (Entry<String, List<RssInfo>> entry : desc.entrySet()) {
+			final List<RssInfo> valueRssInfo = entry.getValue();
+			
+			for (RssInfo rssInfo : valueRssInfo) {
+				System.out.println();
+				System.out.println(rssInfo.getDescription().getInterface());
+			}
+		}
+
 		// RssOption reader = new MyRssReader();
 		// reader.addFeedSorce("https://sports.yahoo.com/top/rss.xml");
 		// reader.addFeedSorce("https://sports.yahoo.com/golf/blog/devil_ball_golf/rss.xml");
@@ -47,7 +56,7 @@ public class DemoRss {
 		// System.out.println(rssInfo.getTitle());
 		// }
 		// reader.serialize();
-		Application.launch(StartDisplay.class, args);
+		// Application.launch(StartDisplay.class, args);
 	}
 
 }
